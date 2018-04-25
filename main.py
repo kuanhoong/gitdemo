e0f6d25c5bc6f1ad96e8ac37eb35ad32e14ba031
Kuan Hoong <kuanhoong@gmail.com>
2018-04-25 16:31:56 +0800

    added master and module

--- main.py
+++
@@ -1,6 +0,0 @@
-## This is the main file
-
-import pandas as pd
-import numpy as np
-
-print("Hello World!")
--- module.py
+++
@@ -1,9 +0,0 @@
-## This is new module
-
-import datetime
-
-print (datetime.date.today())
-
-## add a new function
-
-print (datetime.date.time())
