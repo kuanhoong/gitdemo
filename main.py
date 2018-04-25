db5c3703d3ae943ab42d365ea679510ae457d5e1
Kuan Hoong <kuanhoong@gmail.com>
2018-04-25 08:47:00 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+## This is the main file
+
+import pandas as pd
+import numpy as np
+
+print("Hello World!")
---
+++ module.py
@@ -0,0 +1,5 @@
+## This is new module
+
+import datetime
+
+print (datimetime.date.today())
